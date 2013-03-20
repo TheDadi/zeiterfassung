ef9090a40e5e900d31f88a68dcc0a953ccfdb9db
Rémy Schumm <remy.schumm@axa.ch>
2013-03-20 13:49:19 +0100

    Kleinzeugs

--- stundenabrechnung.bat
+++ stundenabrechnung.bat
@@ -5,9 +5,9 @@ set JBOSS_BIN_DIR=D:\SCRE_Library\jboss-as-7.1.1.Final\bin
 set JBOSS_PORT_OFFSET=1
 
 echo Sarting Database
-start /b java -cp D:\SCRE_Library\hsqldb\lib\hsqldb.jar org.hsqldb.Server -database.0 file:%DATABASE_DIR% -dbname.0 stundenabrechnung
+start /b java -cp D:\SCRE_Library\hsqldb-2.2.9\hsqldb-2.2.9\hsqldb\lib\hsqldb.jar org.hsqldb.Server -database.0 file:%DATABASE_DIR% -dbname.0 stundenabrechnung
 
 sleep 3
 
 echo Starting JBoss
-rem %JBOSS_BIN_DIR%\standalone.bat -Djboss.socket.binding.port-offset=%JBOSS_PORT_OFFSET%
+%JBOSS_BIN_DIR%\standalone.bat -Djboss.socket.binding.port-offset=%JBOSS_PORT_OFFSET%
